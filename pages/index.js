60856f2c1c8c788954a491fb565efaaa56410445
Lumworker <Lumworker@hotmail.com>
2023-11-26 14:16:28 +0700

    auth with google

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import LoginBtn from "@/components/login-btn";
+
+export default function Home() {
+  return (
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <LoginBtn />
+    </div>
+  );
+}
